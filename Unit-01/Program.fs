03d1d262032e9b317fe576dad4c1b3cad0bb4115
Martin Kelly <mk@martinkelly.com>
2021-01-05 14:23:49 -0500

    After lesson 3

--- Unit-01/Program.fs
+++ Unit-01/Program.fs
@@ -8,5 +8,7 @@ let from whom =
 [<EntryPoint>]
 let main argv =
     let message = from "F#" // Call the function
+    let items =argv.Length
     printfn "Hello world %s" message
+    printfn "Passed in %d items: %A" items argv
     0 // return an integer exit code
---
+++ Unit-01/scratchpad.fsx
@@ -0,0 +1,15 @@
+﻿//let text ="Hello, world"
+//text.Length
+
+let greetPerson name age =
+    sprintf "Hello, %s. You are %d years old" name age 
+
+let greeting = greetPerson "Fred" 25
+
+let countWords (text:string) =
+    let words = text.Split(' ')
+    let items = words.Length
+    let filetext = sprintf "There are %d words in the string %s" items text
+    System.IO.File.WriteAllText(@"c:\temp\writetext.txt", filetext)
+
+countWords "This is a bunch of words"
